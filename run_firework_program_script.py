68e2c53171c5330be62686f890a033e6a8461586
korombus <korombus@users.noreply.github.com>
2021-11-25 01:12:08 +0900

    再生用ファイルをjsonファイルからpythonファイルに入れ替え。

--- run_firework_program_script.py
+++ run_firework_program_script.py
@@ -13,8 +13,8 @@ animatio_file_name = FILE_ROOT_PATH + "firework_animation.py"
 woshader_file_name = FILE_ROOT_PATH + "world_shader.py"
 hanamodl_file_name = FILE_ROOT_PATH + "hanabi_model.py"
 
-# 再生用jsonファイル
-program_json_name = FILE_ROOT_PATH + "firework_program.json"
+# 再生用ファイル
+firework_program_file_name = FILE_ROOT_PATH + "firework_program.py"
 
 # SEファイルパス一覧
 SE_ROOT_PATH = FILE_ROOT_PATH + 'se/'
@@ -49,35 +49,30 @@ bpy.data.objects[0].name = "Camera"
 exec(compile(open(woshader_file_name).read(), woshader_file_name, 'exec'))
 bpy.context.scene.frame_end = FRAME_END
 
-# 再生用のjsonファイルを読み込んで、アニメーションに反映
-with open(program_json_name, mode='r', encoding='utf-8') as f:
-    firework_program = json.load(f)
+# 再生用のファイルを読み込んで、アニメーションに反映
+firework_program = {}
+exec(compile(open(firework_program_file_name).read(), firework_program_file_name, 'exec'))
 
-    # フレームごとにkeyデータを読み出し
-    for frame_key in firework_program.keys():
-        # key毎のデータを設定
-        for program_data in firework_program[frame_key]:
-            # 花火の色をランダムに設定
-            firework_color = (random.random(), random.random(), random.random(), 1)
-
-            # パーティクルに使用するオブジェクトのマテリアル作成
-            exec(compile(open(material_file_name).read().replace("ANIM_COLOR", str(firework_color)), material_file_name, 'exec'))
-
-            # パーティクルオブジェクトの名前を保持
-            firework_particle_object_name = bpy.context.active_object.name
-            
-            # 花火本体を生成
-            exec(compile(open(hanamodl_file_name).read().replace('p_x', str(program_data["x"]))
-                                                        .replace('p_y', str(program_data["y"])), hanamodl_file_name, 'exec'))
-
-            # 花火の発射フレームを設定
-            firework_start_frame = frame_key
-
-            # 花火パーティクル作成
-            exec(compile(open(particle_file_name).read().replace("OBJ_NAME", firework_particle_object_name)
-                                                        .replace("ST_FR", str(firework_start_frame)), particle_file_name, 'exec'))
-
-            # 花火アニメーション作成
-            exec(compile(open(animatio_file_name).read().replace("ST_FR", str(firework_start_frame))
-                                                        .replace("SE_BG", str(sound_begin))
-                                                        .replace("SE_BO", str(sound_bomb)), animatio_file_name, 'exec'))
+# フレームごとにkeyデータを読み出し
+for frame_key in firework_program.keys():
+    # key毎のデータを設定
+    for program_data in firework_program[frame_key]:
+        # 花火の色をランダムに設定
+        firework_color = (random.random(), random.random(), random.random(), 1)
+        # パーティクルに使用するオブジェクトのマテリアル作成
+        exec(compile(open(material_file_name).read().replace("ANIM_COLOR", str(firework_color)), material_file_name, 'exec'))
+        # パーティクルオブジェクトの名前を保持
+        firework_particle_object_name = bpy.context.active_object.name
+        
+        # 花火本体を生成
+        exec(compile(open(hanamodl_file_name).read().replace('p_x', str(program_data["x"]))
+                                                    .replace('p_y', str(program_data["y"])), hanamodl_file_name, 'exec'))
+        # 花火の発射フレームを設定
+        firework_start_frame = frame_key
+        # 花火パーティクル作成
+        exec(compile(open(particle_file_name).read().replace("OBJ_NAME", firework_particle_object_name)
+                                                    .replace("ST_FR", str(firework_start_frame)), particle_file_name, 'exec'))
+        # 花火アニメーション作成
+        exec(compile(open(animatio_file_name).read().replace("ST_FR", str(firework_start_frame))
+                                                    .replace("SE_BG", str(sound_begin))
+                                                    .replace("SE_BO", str(sound_bomb)), animatio_file_name, 'exec'))
